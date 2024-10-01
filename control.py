0283e73892eb4fc0f76805ac04c8e0ce6c67cc67
Joshua Leaper <poshernater@outlook.com>
2024-10-02 00:57:28 +0930

    Revert control to basics

--- control.py
+++ control.py
@@ -6,7 +6,6 @@ from commands import Commands
 class Control:
     def __init__(self, client: Client, commands: Commands):
         self.__client = client
-
         self.__commands = commands
 
     async def send_message(self, message: Message) -> None:
@@ -17,34 +16,33 @@ class Control:
             return
 
         response: str = await self.get_response(message)
-        if response:
+        if response != False:
             await message.channel.send(response)
 
-        # finds the appropriate function to call based on the message content
-        async def get_response(self, message: Message):
-            message_content = message.content.lower()
-            if self.__commands.inConvo:
-                if message.author.id == self.__commands.currentUserID:
-                    return await self.__commands.currentFunc(message)
+    # finds the appropraite function to call based on the message content
+    async def get_response(self, message: Message):
+        message_content = message.content.lower()
+        if self.__commands.inConvo == True and message.author.id == self.__commands.currentUserID:
+            return await self.__commands.currentFunc(message)
 
-            elif message_content[0] != '!':
-                return False
-            elif message_content.startswith('!member'):
-                return await self.__commands.add_role(message)
-            elif message_content.startswith('!spam'):
-                return await self.__commands.renew(message)
-            elif message_content.startswith('!msgnonmembers'):
-                return await self.__commands.msg_non_members(message)
-            elif message_content.startswith('!help'):
-                return await self.__commands.help()
-            elif message_content.startswith('!set event'):
-                return await self.__commands.set_event(message)
-            elif message_content.startswith('!see events'):
-                return await self.__commands.display_events()
-            elif message_content.startswith('!del event'):
-                return await self.__commands.delete_event(message)
-            elif message_content.startswith('!ping'):
-                return await self.__commands.ping()
-            elif message_content.startswith('!debug'):
-                return await self.__commands.debug()
+        elif message_content[0] != '!':
+            return False
+        elif message_content.startswith('!member'):
+            return await self.__commands.add_role(message)
+        elif message_content.startswith('!spam'):
+            return await self.__commands.renew(message)
+        elif message_content.startswith('!msgnonmembers'):
+            return await self.__commands.msg_non_members(message)
+        elif message_content.startswith('!help'):
+            return await self.__commands.help()
+        elif message_content.startswith('!set event'):
+            return await self.__commands.set_event(message)
+        elif message_content.startswith('!see events'):
+            return await self.__commands.display_events()
+        elif message_content.startswith('!del event'):
+            return await self.__commands.delete_event(message)
+        elif message_content.startswith('!ping'):
+            return await self.__commands.ping()
+        elif message_content.startswith('!debug'):
+            return await self.__commands.debug()
 
